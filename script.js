c089b7d150884922a99040ee55e315a5c5948874
GorillePanda <mathieuchaillot@gmail.com>
2024-11-24 23:12:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ let compteur = parseInt(localStorage.getItem('compteur')) || 0;
 // Mettre à jour le compteur dans l'élément HTML dès le chargement de la page
 document.getElementById("compteur").textContent = compteur;
 
+// Récupérer le compteur global au chargement de la page
+fetch('/api/compteur')
+    .then(response => response.json())
+    .then(data => {
+    // Afficher le compteur global sur la page
+    document.getElementById("compteurGlobal").textContent = data.totalClics;
+})
+.catch(err => console.error("Erreur lors de la récupération du compteur global", err));
+
 // Fonction pour incrémenter le compteur et jouer le son
 function pop() {
     // Incrémenter le compteur
@@ -19,14 +28,6 @@ function pop() {
     // Créer un objet Audio pour jouer un son à chaque clic
     let audio = new Audio('slurp.mp3');
     audio.play();
-    // Récupérer le compteur global au chargement de la page
-    fetch('/api/compteur')
-        .then(response => response.json())
-        .then(data => {
-        // Afficher le compteur global sur la page
-        document.getElementById("compteurGlobal").textContent = data.totalClics;
-    })
-    .catch(err => console.error("Erreur lors de la récupération du compteur global", err));
 }
 function depop() {
     // Changer l'image quand le clic est relâché
