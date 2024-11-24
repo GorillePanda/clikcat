1d50eaac14ebb750f00902d1a3565f0e97d23f68
GorillePanda <mathieuchaillot@gmail.com>
2024-11-24 21:18:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,10 +26,7 @@ function incrementer(false) {
     // Mettre à jour l'affichage du compteur
     document.getElementById("compteur").textContent = compteur;
     // Changer l'image quand le clic est enfoncé
-    document.getElementById("clikcat").src = 'slurp.jpeg';
-    // Créer un objet Audio pour jouer un son à chaque clic
-    let audio = new Audio('slurp.mp3');
-    audio.play();
+    document.getElementById("clikcat").src = 'cat.jpeg';
     } else {
     // Changer l'image quand le clic est relaché
     document.getElementById("clikcat").src = 'cat.jpeg';
