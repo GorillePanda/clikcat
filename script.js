c5b216093b7f4de8ea7114aaff2f80c56eade50a
GorillePanda <mathieuchaillot@gmail.com>
2024-11-24 18:35:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,4 @@ function changerImage() {
         // Revenir à l'image d'origine lorsque le clic est relâché
         document.getElementById("clikcat").src = 'cat.jpeg';
     }
-    else (false) {
-        // Revenir à l'image d'origine lorsque le clic est relâché
-        document.getElementById("clikcat").src = 'cat.jpeg';
-    }
 }
