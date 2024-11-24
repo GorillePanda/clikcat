12d268f06d0455e59d157412eb23d2c88f96712b
GorillePanda <mathieuchaillot@gmail.com>
2024-11-24 17:21:35 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+
+let compteur = 0;
+
+// Fonction pour incrémenter le compteur et jouer le son
+function incrementer() {
+    // Incrémenter le compteur
+    compteur++;
+    // Mettre à jour l'affichage du compteur
+    document.getElementById("compteur").textContent = compteur;
+
+    // Créer un objet Audio pour jouer un son à chaque clic
+    let audio = new Audio('pop_sound.mp3');
+    audio.play();
+}
