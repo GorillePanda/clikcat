18f9a784f7fe91f71bdef7f643a9bb7c88d92852
GorillePanda <mathieuchaillot@gmail.com>
2024-11-24 19:26:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 // script.js
 
 let compteur = 0;
-    
-   // Fonction pour changer l'image en fonction de l'état du clic
-function changerImage() {
-    // Incrémenter le compteur lorsque l'utilisateur maintient le clic
-    if (true) {
-        compteur++;
-        document.getElementById("compteur").textContent = compteur;
-        // Changer l'image quand le clic est enfoncé
-        document.getElementById("clikcat").src = 'slurp.jpeg';
-        // Créer un objet Audio pour jouer un son à chaque clic
-        let audio = new Audio('slurp.mp3');
-        audio.play();
-    } if (false) {
-        // Revenir à l'image d'origine lorsque le clic est relâché
-        document.getElementById("clikcat").src = 'cat.jpeg';
-    }
+
+// Fonction pour incrémenter le compteur et jouer le son
+function incrementer() {
+    // Incrémenter le compteur
+    compteur++;
+    // Mettre à jour l'affichage du compteur
+    document.getElementById("compteur").textContent = compteur;
+    // Changer l'image quand le clic est enfoncé
+    document.getElementById("clikcat").src = 'slurp.jpeg';
+    // Créer un objet Audio pour jouer un son à chaque clic
+    let audio = new Audio('slurp.mp3');
+    audio.play();
 }
