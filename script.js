266dcd9d60c45efd3a067292a259b044a12af0ae
GorillePanda <mathieuchaillot@gmail.com>
2024-11-24 18:15:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,6 @@
 
 let compteur = 0;
 let imageActuelle = 'cat.jpeg'; // Image initiale
-
-// Fonction pour incrémenter le compteur et jouer le son, et changer l'image
-function incrementer() {
-    // Incrémenter le compteur
-    compteur++;
-    // Mettre à jour l'affichage du compteur
-    document.getElementById("compteur").textContent = compteur;
-
-    // Créer un objet Audio pour jouer un son à chaque clic
-    let audio = new Audio('slurp.mp3');
-    audio.play();
     
    // Fonction pour changer l'image en fonction de l'état du clic
 function changerImage() {
