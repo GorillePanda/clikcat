ee26ae44b1e9781d6318baa29f63f76c79daf666
GorillePanda <mathieuchaillot@gmail.com>
2024-11-27 17:06:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 
 // Récupérer le compteur de clics depuis le localStorage (s'il existe), sinon initialiser à 0
 let compteur = parseInt(localStorage.getItem('compteur')) || 0;
+let compteur = parseInt(localStorage.getItem('compteur2')) || 0;
 
 // Mettre à jour le compteur dans l'élément HTML dès le chargement de la page
 document.getElementById("compteur").textContent = compteur;
+document.getElementById("compteur2").textContent = compteur;
 
 // Fonction pour incrémenter le compteur et jouer le son
 function pop() {
